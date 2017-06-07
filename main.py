1bdafe73a3705512be6b50bbba62da8268e8299b
Oleg Yamnikov <bgeyts667@gmail.com>
2017-06-07 23:14:15 +0500

    Add some simple logging into stdout

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ def on_query(bot: Bot, update: Update):
     if not inline_query:
         return
 
+    print("Inline query from {}:{} with text '{}'".format(
+        inline_query.from_user.id, inline_query.from_user.first_name,
+        inline_query.query))
+
     stickers = search_stickers(inline_query.query)
 
     # No more than 50 results are allowed by telegram bot api.
@@ -67,16 +71,24 @@ def on_message(bot: Bot, update: Update):
     sticker_is_in_db = is_sticker and message.sticker.file_id in config.STICKERS
 
     if sticker_is_in_db:
+        print("Message from {}:{} with known sticker '{}'".format(
+            message.from_user.id, message.from_user.first_name,
+            message.sticker.file_id))
         bot.send_message(
             message.chat.id,
             config.HYPE_MSG,
             parse_mode='Markdown')
     elif is_sticker:
+        print("Message from {}:{} with unknown sticker '{}'".format(
+            message.from_user.id, message.from_user.first_name,
+            message.sticker.file_id))
         bot.send_message(
             message.chat.id,
             config.STICKER_DATA_MSG.format(file_id=message.sticker.file_id),
             parse_mode='Markdown')
     else:
+        print("Message from {}:{} with text '{}'".format(
+            message.from_user.id, message.from_user.first_name, message.text))
         bot.send_message(
             message.chat.id,
             config.INSTRUCTIONS_MSG.format(stickers_count=len(config.STICKERS)),
