5cce8871522d9a70bdab8277dee79d63a8d64883
Oleg Yamnikov <bgeyts667@gmail.com>
2017-06-07 21:52:28 +0500

    Add first huge sticker pack in to db
    Yes I did it. I commited data into the source control. Don;t judge me.
    This data won't change anyway and I would want it to be versioned.

--- config/stickers.py
+++ config/stickers.py
@@ -3,8 +3,393 @@
 #
 # To get file id of an arbitrary sticker, just send it to the bot.
 STICKERS = {
-    'CAADAgADFQYAAgi3GQInJ_dji2gMHwI': [
-        "you know nothing jon snow",
-        "ничего ты не знаешь джон сноу",
-    ]
+    # Sticker pack by Nikita Popov and Yuriy Soloshenko. Volume 1.
+    # t.me/UpToHype
+    'CAADAgADlwIAAtGzAgABPRjZ2y91ieMC': [
+        "хайпанем немножечко",
+        "хайпанем немножко",
+    ],
+    'CAADAgADmAIAAtGzAgABp6kfHwXFhUoC': [
+        "логично не правда ли",
+        "логично неправда ли",
+    ],
+    'CAADAgADmQIAAtGzAgABaUe5IhyC7JEC': [
+        "я обратился к потокам своего сознания",
+        "потоки сознания",
+    ],
+    'CAADAgADmgIAAtGzAgABwPkQol7u23sC': [
+        "печальная история",
+    ],
+    'CAADAgADnAIAAtGzAgABeClmyrg3cHoC': [
+        "вкладывать свои лайки в эти мемы я не советую",
+    ],
+    'CAADAgADngIAAtGzAgABuZWIUm895ggC': [
+        "вечна только любовь",
+    ],
+    'CAADAgADnwIAAtGzAgABPvANri27ga8C': [
+        "кому это выгодно"
+    ],
+    'CAADAgADoAIAAtGzAgABzr4ctIqrrPkC': [
+        "во всем виноват алексей навальный"
+    ],
+    'CAADAgADqQIAAtGzAgABF7napJrfId4C': [
+        "добавьте немножечко позитива",
+        "добавьте немножко позитива",
+    ],
+    'CAADAgADvAIAAtGzAgAB-Lm3g6n9zo8C': [
+        "это вишенка на торте хайпа",
+        "это вишня на торте хайпа",
+    ],
+    'CAADAgADvgIAAtGzAgABxe4IQNZmvgEC': [
+        "скорее всего это какой-то заговор",
+    ],
+    'CAADAgADvwIAAtGzAgAB3UC3tEjwcRIC': [
+        "я провел собственное расследование",
+        "я провел собственное раследование",
+    ],
+    'CAADAgADqAIAAtGzAgABNCxho_FRbtMC': [
+        "воровать не плохо",
+        "воровать неплохо",
+    ],
+    'CAADAgADwAIAAtGzAgABN3hYlKnzl-cC': [
+        "приятного просмотра",
+    ],
+    'CAADAgADwQIAAtGzAgABrKzaDBKhXKkC': [
+        "на этом все",
+        "на этом всё",
+    ],
+    'CAADAgADgAIAAtGzAgABq7g7Q1wKSmkC': [
+        "продолжаем хайпить",
+    ],
+    'CAADAgADgQIAAtGzAgABlkBCkNHmj48C': [
+        "даб", "дэб", "деб",
+    ],
+    'CAADAgADggIAAtGzAgAB1kGrOwABUaLYAg': [
+        "ничто не вечно",
+        "ничто невечно",
+    ],
+    'CAADAgADwgIAAtGzAgABjqCKqSI-He8C': [
+        "я с ужасом все осознал",
+    ],
+    'CAADAgADgwIAAtGzAgABVQMeb0CTVvEC': [
+        "с этим вопросом мне пришлось жить 2 минуты",
+        "с этим вопросом мне пришлось жить две минуты",
+    ],
+    'CAADAgADhAIAAtGzAgAB2t9E9Q5Gb4wC': [
+        "дратути",
+    ],
+    'CAADAgADhQIAAtGzAgABHRP-tjIQJ5YC': [
+        "не лайкайте это похотливое животное",
+    ],
+    'CAADAgADCwMAAtGzAgABcr2eFT4U7o8C': [
+        "данное видео фейк",
+        "данное видео фэйк",
+    ],
+    'CAADAgADoQIAAtGzAgABHGPR-a-h1kEC': [
+        "сердцу не прикажешь",
+    ],
+    'CAADAgADhwIAAtGzAgAB_VzfYXEvhz0C': [
+        "хватит обманывать людей",
+    ],
+    'CAADAgADiAIAAtGzAgABZUc24LJqlSsC': [
+        "за репост не сажают",
+    ],
+    'CAADAgADiQIAAtGzAgAB6cxunB4sCesC': [
+        "черно-белый фон",
+    ],
+    'CAADAgADigIAAtGzAgAB7S5NG2WtFOQC': [
+        "я держу в руках весь российский интернет"
+    ],
+    'CAADAgADiwIAAtGzAgABoCXVJdt8n1oC': [
+        "хорошая штука",
+    ],
+    'CAADAgADwwIAAtGzAgABU8jGfU8QTQoC': [
+        "ага-ага",
+    ],
+    'CAADAgADjAIAAtGzAgABlP6B9dkU0ncC': [
+        "кто не понял тот поймет",
+    ],
+    'CAADAgADogIAAtGzAgABmrThoFuZhDYC': [
+        "это знают даже дети",
+    ],
+    'CAADAgADjQIAAtGzAgABU1Kv18IC0qoC': [
+        "у меня получилось",
+    ],
+    'CAADAgADjwIAAtGzAgABd2fbwKrbEtAC': [
+        "надо быть в тренде",
+    ],
+    'CAADAgADkAIAAtGzAgABrG8vI1gx4AUC': [
+        "кто все эти люди",
+    ],
+    'CAADAgADpQIAAtGzAgABfUQsLdR61w4C': [
+        "полезная информация",
+    ],
+    'CAADAgADkQIAAtGzAgAB3UfI21slWiUC': [
+        "мне пришлось подключить свою чакру и wi-fi",
+        "мне пришлось подключить свою чакру и вай-фай",
+    ],
+    'CAADAgADkgIAAtGzAgABYxN8UvbJ9aEC': [
+        "люблю науку",
+    ],
+    'CAADAgADkwIAAtGzAgAB4n9uTZ8-gOYC': [
+        "меньше доверяйте превью",
+    ],
+    'CAADAgADowIAAtGzAgABA1AvoPE1Dt4C': [
+        "простите я наврал",
+    ],
+    'CAADAgADlAIAAtGzAgABHb2fp5kcv8UC': [
+        "шутка",
+    ],
+    'CAADAgADqgIAAtGzAgABu6Iofljkxo8C': [
+        "приправим лайками этот видосик",
+        "приправим лайками это видео",
+    ],
+    'CAADAgADqwIAAtGzAgABk97aNRwt23cC': [
+        "мой черенок сознания ударился о твердую правду",
+        "мой черенок сознания ударился об твердую правду",
+    ],
+    'CAADAgADrAIAAtGzAgAB-nEm_-h_N94C': [
+        "изи",
+        "easy",
+    ],
+    'CAADAgADrQIAAtGzAgABr_PnX-ABv2MC': [
+        "люби предков",
+    ],
+    'CAADAgADrgIAAtGzAgABUhtGXXpOQZsC': [
+        "а ведь мог бы неплохо зарабатывать",
+    ],
+    'CAADAgADrwIAAtGzAgABCCPmP9ovFnEC': [
+        "тратьте кеш",
+        "тратьте кэш",
+    ],
+    'CAADAgADsAIAAtGzAgABRHH8fr73-yUC': [
+        "лайки крутятся мемасы мутятся",
+    ],
+    'CAADAgADsQIAAtGzAgABFcund0w0vAcC': [
+        "глупцы",
+    ],
+    'CAADAgADsgIAAtGzAgABrNKMKrYW_EQC': [
+        "что это было",
+    ],
+    'CAADAgADswIAAtGzAgABeMQtbymQC0QC': [
+        "от проблем не убежишь",
+    ],
+    'CAADAgADtAIAAtGzAgABwyXB6-wnVIEC': [
+        "это была просто ностальгия",
+    ],
+    'CAADAgADtQIAAtGzAgAB_dotnmTZy8oC': [
+        "согласен звучит как сценарий плохого фильма",
+        "плохой фильм",
+    ],
+    'CAADAgADtwIAAtGzAgABUdEUZBDV-xMC': [
+        "я занимаюсь не тем",
+        "я занимаюсь нетем",
+    ],
+    'CAADAgADuAIAAtGzAgABr04pszmHVMsC': [
+        "будь самодостаточен",
+    ],
+    'CAADAgADuQIAAtGzAgABjO9QN0U4EVoC': [
+        "очки", "в очках", "очонки нннада",
+    ],
+    'CAADAgADxQIAAtGzAgABgEikPB8BUkkC': [
+        "настоящий батя в здании",
+    ],
+    'CAADAgAD4AIAAtGzAgAB4R164-xtf-kC': [
+        "разве это возможно без фотошопа",
+    ],
+    'CAADAgADxwIAAtGzAgABjvrGgOM9D7sC': [
+        "отдыхай в крыму",
+    ],
+    'CAADAgADyAIAAtGzAgABi8MaCVp7-rwC': [
+        "как настоящий мужчина я должен сделать первый шаг",
+    ],
+    'CAADAgAD3wIAAtGzAgABFLHFiIDUYcsC': [
+        "но делать я это конечно же не буду",
+        "но делать я это конечноже не буду",
+    ],
+    'CAADAgADygIAAtGzAgABCzXt_Ot2HW4C': [
+        "нужно больше золота",
+    ],
+    'CAADAgADywIAAtGzAgABKK31AAEnSbHhAg': [
+        "мне не нужны проблемы с законом",
+    ],
+    'CAADAgADzAIAAtGzAgABPsZKd7ZtdloC': [
+        "это заставило мои извилины работать 24/7",
+        "это заставило мои извилины работать двадцать четыре на семь",
+    ],
+    'CAADAgADzQIAAtGzAgABZTMwlg_VvEsC': [
+        "поживем увидим",
+        "поживём увидим",
+    ],
+    'CAADAgADzgIAAtGzAgABr-WOVBa6JKwC': [
+        "я не рептилоид люблю макароны и теории заговора",
+    ],
+    'CAADAgADzwIAAtGzAgABiobO2wFkEqUC': [
+        "одним словом потрачено",
+    ],
+    'CAADAgAD0AIAAtGzAgAB5KgoT8wPuyIC': [
+        "от любви до ненависти один лайк",
+    ],
+    'CAADAgAD0QIAAtGzAgABZASwkCypozkC': [
+        "без лола и кека не бывает чебурека",
+    ],
+    'CAADAgADCgMAAtGzAgABnCYDVY3bbSoC': [
+        "хаю хай с вами не иван гай",
+    ],
+    'CAADAgAD0gIAAtGzAgAB7hvCvSDs1zwC': [
+        "вот это да",
+    ],
+    'CAADAgAD0wIAAtGzAgABqRDzeO1ZqCYC': [
+        "ишь ты",
+    ],
+    'CAADAgAD3AIAAtGzAgAB4Ir984v3590C': [
+        "т уважение",
+    ],
+    'CAADAgAD1AIAAtGzAgAB-K3oS0bSVyIC': [
+        "креативный ответ",
+    ],
+    'CAADAgAD1QIAAtGzAgABBctFeh_NrQcC': [
+        "мейк ютюб грейт эгейн",
+        "мэйк ютюб грэйт эгейн",
+        "make youtube great again",
+    ],
+    'CAADAgAD2AIAAtGzAgABr0n1qa0hzNYC': [
+        "за мной стоит власть",
+    ],
+    'CAADAgAD1gIAAtGzAgABWUEGrh8kEvAC': [
+        "схема работает",
+    ],
+    'CAADAgAD3gIAAtGzAgABZb2zS2KM2usC': [
+        "жиза",
+    ],
+    'CAADAgAD3QIAAtGzAgABbWKV7Dc5e-AC': [
+        "я выбираю жить в кайф",
+        "я выбираю жить в каеф",
+    ],
+    'CAADAgAD2QIAAtGzAgABXgFV1s-VjTkC': [
+        "не всем удача улыбается своей золотой ухмылкой",
+    ],
+    'CAADAgAD4QIAAtGzAgABM2yiri4xxp4C': [
+        "я честный человек и не репер",
+        "я честный человек и не рэпер",
+        "я чесный человек и не репер",
+        "я чесный человек и не рэпер",
+    ],
+    'CAADAgAD4gIAAtGzAgABiF-c_9DwsQYC': [
+        "я его не пользую я его формирую",
+    ],
+    'CAADAgAD4wIAAtGzAgABRxWoSc7Zg4cC': [
+        "ты хайпожор",
+    ],
+    'CAADAgAD5AIAAtGzAgABEd-B0ldW1jAC': [
+        "почувствуй мою любовь",
+        "почуствуй мою любовь",
+    ],
+    'CAADAgAD5QIAAtGzAgABX7cLvZvyd-kC': [
+        "я хочу совершить финансовый каминг-аут",
+    ],
+    'CAADAgAD5gIAAtGzAgABu2XLqYudnOcC': [
+        "рашим посоны",
+        "рашим пацаны",
+    ],
+    'CAADAgAD8wIAAtGzAgABzDKp2O4Js54C': [
+        "охуенно сказано",
+    ],
+    'CAADAgAD6AIAAtGzAgAB_qO9S6fSV4sC': [
+        "нас связывают невидимые нити",
+    ],
+    'CAADAgAD6QIAAtGzAgABNqXMrSnvRJkC': [
+        "курлык курлык",
+    ],
+    'CAADAgAD6gIAAtGzAgABkkKJs6AamFkC': [
+        "не подорви мое доверие",
+    ],
+    'CAADAgAD6wIAAtGzAgABWA9gEC2TrrgC': [
+        "а что мне понравилось",
+    ],
+    'CAADAgAD7AIAAtGzAgABjN7Gj532_JMC': [
+        "кто эти новоявленные цезари",
+    ],
+    'CAADAgAD7QIAAtGzAgABcJKpG_Q0XFIC': [
+        "камон",
+        "come on",
+    ],
+    'CAADAgAD7gIAAtGzAgABft4QW2S2QP4C': [
+        "азаза",
+    ],
+    'CAADAgAD7wIAAtGzAgAB4CqztNAoMjAC': [
+        "я 2 раза не повторяю",
+        "я два раза не повторяю",
+    ],
+    'CAADAgAD8AIAAtGzAgABVtpC4gIZ6UAC': [
+        "я принял волевое решение",
+    ],
+    'CAADAgADCQMAAtGzAgABzd7IlRavrrMC': [
+        "я испытал холод стыд брызги от автомобилей",
+        "я испытал холод стыд брызги от машин",
+    ],
+    'CAADAgAD8gIAAtGzAgABW2xRdbc_PqoC': [
+        "разлагайся и деградируй",
+    ],
+    'CAADAgAD-AIAAtGzAgABgzC_BFmXohwC': [
+        "вотэфэ",
+        "вотефе",
+        "втф",
+        "wtf",
+    ],
+    'CAADAgAD-QIAAtGzAgABvVdIK-urAmgC': [
+        "под гнетом этих вопросов нейроны заискрились в моей голове",
+    ],
+    'CAADAgAD-gIAAtGzAgABKxT5Rf89lowC': [
+        "от этого я чуть было не заорал",
+    ],
+    'CAADAgAD-wIAAtGzAgABCWMBDMYrMX4C': [
+        "я запутался",
+    ],
+    'CAADAgAD_AIAAtGzAgABbLAnFWI_iBUC': [
+        "это сглаз",
+    ],
+    'CAADAgADCAMAAtGzAgABtotgaUVpu-4C': [
+        "ни пуха ни пера",
+        "не пуха не пера",
+    ],
+    'CAADAgAD_gIAAtGzAgABqayyLIU0nTMC': [
+        "значит не сглаз",
+    ],
+    'CAADAgAD_wIAAtGzAgABk9wny7--UyYC': [
+        "дело в хайпе",
+    ],
+    'CAADAgAEAwAC0bMCAAFRM8k2AAGP9cQC': [
+        "ле гоу",
+        "летс гоу",
+        "let s go",
+    ],
+    'CAADAgADEQMAAtGzAgABaODXZVN1Q': [
+        "мои поиски закончены",
+    ],
+    'CAADAgADAQMAAtGzAgABnudGrtiqLKgC': [
+        "сяп", "пасиб", "спс", "спасибо",
+        "сап", "вотсап", "what s up",
+    ],
+    'CAADAgADAgMAAtGzAgAB6iFC6B2SBUEC': [
+        "примерим корону хайпа на себя",
+    ],
+    'CAADAgADAwMAAtGzAgABQWvtdh-Xl1AC': [
+        "кек коронный",
+    ],
+    'CAADAgADBAMAAtGzAgABKfY6TRcwVf8C': [
+        "лол похоронный"
+    ],
+    'CAADAgADBQMAAtGzAgABBFkX_uyvELMC': [
+        "я что дура",
+    ],
+    'CAADAgADBgMAAtGzAgABComYmUAEc5kC': [
+        "улыбка", "черно-белый фон", "смайл",
+    ],
+    "CAADAgADEAMAAtGzAgABZRmHGHlpvsoC": [
+        "мне не нужен хайп",
+    ],
+    'CAADAgADBwMAAtGzAgABxB9ITm_SjxAC': [
+        "лысый рептилоид",
+    ],
 }
